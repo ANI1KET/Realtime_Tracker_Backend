d17e5e206ea911ca4e9830a88f57a015c7f83a1b
ANI1KET <aniketrouniyar12@gmail.com>
2024-07-14 15:39:13 +0530

    Deploy

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import express, { Express } from "express";
-import { PrismaClient } from "@prisma/client";
 import { createServer } from "http";
 import cors from "cors";
 
 import { PORT } from "./env_variable";
-import rootRoute from "./routers/rootRouter";
-import { errorMiddleware } from "./middlewares/errors";
 import { setupSocketIO } from "./socket";
 
 const app: Express = express();
@@ -20,15 +17,8 @@ app.use(
 const sockethttpServer = createServer(app);
 setupSocketIO(sockethttpServer);
 
-export const prismaClient = new PrismaClient({
-  log: ["query"],
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/api", rootRoute);
-app.use(errorMiddleware);
 
 sockethttpServer.listen(PORT, () => {
   console.log(`Server Running on Port : ${PORT}`);
